30b66089ae1dd9c5e82c86e34b6fe3fcd50096e3
Jazper <jazpersaldana@yahoo.com>
2024-06-21 16:32:15 -0700

    dsa stack lab complete

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,31 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return True if len(self.items) == 0 else False
 
     def push(self, item):
-        pass
+        if len(self.items) < self.limit:
+            return self.items.append(item)
 
     def pop(self):
-        pass
+        return self.items.pop() if len(self.items) >= 1 else None
 
     def peek(self):
         pass
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        print(len(self.items), self.limit)
+        return True if len(self.items) == self.limit else False
 
     def search(self, target):
-        pass
+        for index, item in enumerate(self.items[::-1]):
+            if item == target:
+                return index
+        return -1
